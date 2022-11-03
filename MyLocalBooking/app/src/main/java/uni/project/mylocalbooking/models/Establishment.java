2c6f48ef333afd408e868d3d3373faaf518722ff
Nicola Marizza <nicola.marizza@gmail.com>
2022-11-03 10:44:05 +0100

    Establishments

---
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/models/Establishment.java
@@ -0,0 +1,30 @@
+package uni.project.mylocalbooking.models;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Establishment implements IDatabaseModel {
+    private final Long id;
+    public final Provider provider;
+    public final String name;
+    public final String address;
+    public final Coordinates position;
+    public final Collection<SlotBlueprint> blueprints = new ArrayList<>();
+
+    public Establishment(Long id, Provider provider, String name, String address, Coordinates position) {
+        this.id = id;
+        this.provider = provider;
+        this.name = name;
+        this.address = address;
+        this.position = position;
+    }
+
+    public Establishment(Provider provider, String name, String address, Coordinates position) {
+        this(null, provider, name, address, position);
+    }
+
+    @Override
+    public Long getId() {
+        return this.id;
+    }
+}
