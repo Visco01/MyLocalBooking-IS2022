3f25ee259ce20d1f079e46cb8240c7408e1ee064
Nicola Marizza <nicola.marizza@gmail.com>
2022-11-03 11:13:40 +0100

    Provider constructor requires strikedUsers argument
    strikedUsers is a HashMap<Client, Integer> where the integer argument indicates the amount of strikes the user got. If the integer argument is null, it means that the user has been blacklisted

--- MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/models/Provider.java
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/models/Provider.java
@@ -1,7 +1,7 @@
 package uni.project.mylocalbooking.models;
 
 import java.util.Date;
-
+import java.util.HashMap;
 
 public class Provider extends AppUser {
     private final Long id;
@@ -9,16 +9,20 @@ public class Provider extends AppUser {
     public final int maxStrikes;
     public final String companyName;
 
-    public Provider(Long id, boolean verified, Integer maxStrikes, String companyName, Long app_user_id, String cellphone, String email, String firstname, String lastname, Date dob) {
+    public HashMap<Client, Integer> strikedUsers;
+
+    public Provider(Long id, boolean verified, Integer maxStrikes, String companyName, HashMap<Client, Integer> strikedUsers, Long app_user_id, String cellphone, String email, String firstname, String lastname, Date dob) {
         super(app_user_id, cellphone, email, firstname, lastname, dob);
         this.id = id;
         this.verified = verified;
         this.maxStrikes = maxStrikes;
         this.companyName = companyName;
+
+        this.strikedUsers = strikedUsers;
     }
 
-    public Provider(boolean verified, Integer maxStrikes, String companyName, String cellphone, String email, String firstname, String lastname, Date dob) {
-        this(null, verified, maxStrikes, companyName, null, cellphone, email, firstname, lastname, dob);
+    public Provider(boolean verified, Integer maxStrikes, String companyName, HashMap<Client, Integer> strikedUsers, String cellphone, String email, String firstname, String lastname, Date dob) {
+        this(null, verified, maxStrikes, companyName, strikedUsers, null, cellphone, email, firstname, lastname, dob);
     }
 
     @Override
