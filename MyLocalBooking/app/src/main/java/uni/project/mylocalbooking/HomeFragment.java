c775f18b631692ae1e05cd23a8336d5f05307ba0
mircomellara <mirco.mellara@gmail.com>
2022-11-07 13:10:03 +0100

    Fix build
    re-do lavoro precedente, fixato un problema nella build, adattato al package name usato nel branch "development"

---
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/HomeFragment.java
@@ -0,0 +1,34 @@
+package uni.project.mylocalbooking;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+
+    //private FragmentHomeBinding binding;
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
--- MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/MainActivity.java
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/MainActivity.java
@@ -4,11 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+    //private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ----- Navigation ----- //
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, homeFragment).commit();
+                    return true;
+                case R.id.profile:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, profileFragment).commit();
+                    return true;
+            }
+            return false;
+        });
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        // ----- End of Navigation ----- //
+
     }
+
 }
