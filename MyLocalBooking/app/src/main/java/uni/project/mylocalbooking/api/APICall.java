02e5d92e66bb5f031123d931e7e21cab60cb54db
Pietro Visconti <visconti373@gmail.com>
2022-11-13 12:12:51 +0100

    Added switch cases to APICall's constructor

--- MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/api/APICall.java
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/api/APICall.java
@@ -17,9 +17,32 @@ import java.util.Map;
 class APICall {
     private String jwt = null;
     private JsonObjectRequest request = null;
+    private String type = null;
+    private String requestBody = null;
+    private String url = null;
 
-    public APICall(String jwt){
+    public APICall(String jwt, String type, String requestBody, String url){
         this.jwt = jwt;
+        this.type = type;
+        this.requestBody = requestBody;
+        this.url = url;
+
+        switch (this.type){
+            case "GET":
+                //TODO
+                break;
+            case "POST":
+                post(this.requestBody, this.url);
+                break;
+            case "PATCH":
+                //TODO
+                break;
+            case "DELETE":
+                //TODO
+                break;
+            default:
+                Log.e("APICall constructor error", "Missing type");
+        }
     }
 
     private static JSONObject getJsonBody(String body){
@@ -32,9 +55,9 @@ class APICall {
         return jsonBody;
     }
 
-    public void post(String body, String url){
+    private void post(String body, String url){
         JSONObject jsonBody = getJsonBody(body);
-
+        Log.i("post method", body);
         JsonObjectRequest jsonRequest = new JsonObjectRequest(
                 Request.Method.POST,
                 url,
@@ -48,15 +71,19 @@ class APICall {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.i("auth request", error.toString());
+                        Log.i("post error", error.toString());
                     }
                 }
         ) {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
                 HashMap<String, String> headers = new HashMap<String, String>();
-                //headers.put("Content-Type", "application/json");
-                headers.put("Authorization", "Bearer "+ jwt);
+                headers.put("Authorization", "Bearer " + jwt);
+                headers.put("Content-Type", "application/json");
+                headers.put("User-Agent", "Mozilla/5.0");
+                headers.put("Accept", "*/*");
+                headers.put("Accept-Encoding", "gzip, deflate, br");
+                headers.put("Connection", "keep-alive");
                 return headers;
             }
         };
@@ -66,5 +93,9 @@ class APICall {
 
     public JsonObjectRequest getRequest(){
         return this.request;
+    }
+
+    public String getType(){
+        return this.type;
     }
 }
