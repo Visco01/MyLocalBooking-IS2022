c7fd92a0e5bbe1e228ff8fe4f21bff00f39c5ebc
Nicola Marizza <nicola.marizza@gmail.com>
2022-11-16 21:07:54 +0100

    Adapter supports different item types

--- MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/activites/client/SlotListAdapter.java
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/activites/client/SlotListAdapter.java
@@ -7,11 +7,13 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import uni.project.mylocalbooking.R;
 import uni.project.mylocalbooking.models.ISelectableSlot;
+import uni.project.mylocalbooking.models.ITimeFrame;
 import uni.project.mylocalbooking.models.ManualSlot;
 import uni.project.mylocalbooking.models.ManualSlotBlueprint;
 import uni.project.mylocalbooking.models.PeriodicSlot;
@@ -21,18 +23,51 @@ import uni.project.mylocalbooking.models.SlotBlueprint;
 public class SlotListAdapter extends BaseAdapter {
     public interface IListener {
         void onSlotReservationToggled(ISelectableSlot slot);
+        void onManualSlotCreate(ITimeFrame timeFrame);
     }
 
     private final IListener listener;
-    private final List<ISelectableSlot> filteredSlots = new ArrayList<>();
+    private final List<ITimeFrame> filteredSlots = new ArrayList<>();
 
     public SlotListAdapter(IListener listener) {
         super();
         this.listener = listener;
     }
+
+    public void onRefresh(LocalDate selected, List<SlotBlueprint> blueprints) {
+        filteredSlots.clear();
+
+        for(final SlotBlueprint item : blueprints) {
+            if(item instanceof ManualSlotBlueprint) {
+                ManualSlotBlueprint blueprint = (ManualSlotBlueprint) item;
+                filteredSlots.addAll(extractTimeframes(blueprint, selected));
+            } else {
+                PeriodicSlot scheduledForSelected = ((PeriodicSlotBlueprint) item).slots.get(selected);
+                if(scheduledForSelected != null)
+                    filteredSlots.add(scheduledForSelected);
+                else
+                    filteredSlots.add((PeriodicSlotBlueprint) item);
+            }
+        }
+
+        filteredSlots.sort(Comparator.comparing(ITimeFrame::getStart));
+
+        notifyDataSetChanged();
+    }
+
     @Override
     public int getCount() {
         return filteredSlots.size();
+    }
+
+    @Override
+    public int getItemViewType(int i) {
+        return filteredSlots.get(i) instanceof ISelectableSlot ? R.layout.manual_blueprint_list_item : R.layout.slot_list_item;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
     }
 
     @Override
@@ -47,12 +82,67 @@ public class SlotListAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        View slotRoot = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.slot, viewGroup, false);
+        View viewRoot = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.manual_blueprint_list_item, viewGroup, false);
 
-        ISelectableSlot slot = filteredSlots.get(i);
-        ((TextView) slotRoot.findViewById(R.id.from_time)).setText(slot.getFromTime().toString());
-        ((TextView) slotRoot.findViewById(R.id.to_time)).setText(slot.getToTime().toString());
+        if(getItemViewType(i) == R.layout.slot_list_item)
+            createSlotElement(i, viewRoot);
+        else
+            createAvailableTimeWindow(i, viewRoot);
+
+        return viewRoot;
+    }
+
+    private List<ITimeFrame> extractTimeframes(ManualSlotBlueprint blueprint, LocalDate date) {
+        List<ManualSlot> slots = blueprint.slots.get(date);
+        List<ITimeFrame> results = new ArrayList<>();
+
+        if(slots == null) {
+            results.add(blueprint);
+            return results;
+        }
+
+        LocalTime previous = blueprint.openTime;
+        for(ManualSlot slot : slots) {
+            if(previous.compareTo(slot.fromTime) < 0) {
+                long nano = previous.toNanoOfDay();
+                results.add(new ITimeFrame() {
+                    @Override
+                    public LocalTime getStart() {
+                        return LocalTime.ofNanoOfDay(nano);
+                    }
+
+                    @Override
+                    public LocalTime getEnd() {
+                        return slot.fromTime;
+                    }
+                });
+            }
+            results.add(slot);
+            previous = slot.toTime;
+        }
+
+        long nano = previous.toNanoOfDay();
+        if(previous.compareTo(blueprint.closeTime) < 0)
+            results.add(new ITimeFrame() {
+                @Override
+                public LocalTime getStart() {
+                    return LocalTime.ofNanoOfDay(nano);
+                }
+
+                @Override
+                public LocalTime getEnd() {
+                    return blueprint.closeTime;
+                }
+            });
+
+        return results;
+    }
+
+    private void createSlotElement(int i, View slotRoot) {
+        ISelectableSlot slot = (ISelectableSlot) filteredSlots.get(i);
+        ((TextView) slotRoot.findViewById(R.id.from_time)).setText(slot.getStart().toString());
+        ((TextView) slotRoot.findViewById(R.id.to_time)).setText(slot.getEnd().toString());
 
         Integer reservationLimit = slot.getReservationLimit();
         int attending = slot.getAttending() != null ? slot.getAttending().size() : 0;
@@ -96,33 +186,13 @@ public class SlotListAdapter extends BaseAdapter {
             // TODO: toggle style
             listener.onSlotReservationToggled(slot);
         });
-
-        return slotRoot;
     }
 
-    public void onRefresh(LocalDate selected, List<SlotBlueprint> blueprints) {
-        filteredSlots.clear();
+    private void createAvailableTimeWindow(int i, View viewRoot) {
+        ITimeFrame timeFrame = filteredSlots.get(i);
 
-        for(final SlotBlueprint item : blueprints) {
-            if(item instanceof ManualSlotBlueprint) {
-                List<ManualSlot> scheduledForSelected = ((ManualSlotBlueprint) item).slots.get(selected);
-                if(scheduledForSelected == null)
-                    continue;
-
-                for(ManualSlot slot : scheduledForSelected)
-                    filteredSlots.add(slot);
-
-            } else {
-                PeriodicSlot scheduledForSelected = ((PeriodicSlotBlueprint) item).slots.get(selected);
-                if(scheduledForSelected != null)
-                    filteredSlots.add(scheduledForSelected);
-                else
-                    filteredSlots.add((PeriodicSlotBlueprint) item);
-            }
-        }
-
-        filteredSlots.sort(Comparator.comparing(ISelectableSlot::getFromTime));
-
-        notifyDataSetChanged();
+        ((TextView) viewRoot.findViewById(R.id.from_time)).setText(timeFrame.getStart().toString());
+        ((TextView) viewRoot.findViewById(R.id.to_time)).setText(timeFrame.getEnd().toString());
+        ((Button) viewRoot.findViewById(R.id.create_slot_button)).setOnClickListener(view -> listener.onManualSlotCreate(timeFrame));
     }
 }
