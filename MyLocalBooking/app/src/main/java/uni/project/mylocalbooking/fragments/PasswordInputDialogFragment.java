9f82b5291413c43378e92c63a591eb9da4089462
Nicola Marizza <nicola.marizza@gmail.com>
2022-11-18 00:55:12 +0100

    Fixed null listener bug for password input dialog

--- MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/fragments/PasswordInputDialogFragment.java
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/fragments/PasswordInputDialogFragment.java
@@ -2,6 +2,7 @@ package uni.project.mylocalbooking.fragments;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -27,27 +28,23 @@ public class PasswordInputDialogFragment extends DialogFragment {
         this.confirmListener = confirmListener;
         this.cancelListener = cancelListener;
         this.titleId = titleId;
-    }
 
-    public PasswordInputDialogFragment(IConfirmListener confirmListener, int titleId) {
-        this(confirmListener, null, titleId);
-    }
 
-    public PasswordInputDialogFragment(ICancelListener cancelListener, int titleId) {
-        this(null, cancelListener, titleId);
     }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         View view = requireActivity().getLayoutInflater().inflate(R.layout.input_slot_password_layout, null);
+        DialogInterface.OnClickListener confirmCallback = confirmListener == null ? null : (d,i) -> {
+            confirmListener.onSlotPasswordSubmitted(((EditText) view.findViewById(R.id.slot_password_field)).getText().toString());
+        };
+        DialogInterface.OnClickListener cancelCallback = cancelListener == null ? null : (d,i) -> cancelListener.onSlotPasswordCanceled();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setView(view);
         builder.setTitle(titleId)
-                .setPositiveButton(R.string.confirm, (dialog, id) -> {
-                    confirmListener.onSlotPasswordSubmitted(((EditText) view.findViewById(R.id.slot_password_field)).getText().toString());
-                })
-                .setNegativeButton(R.string.cancel, (dialog, id) -> cancelListener.onSlotPasswordCanceled());
+                .setPositiveButton(R.string.confirm, confirmCallback)
+                .setNegativeButton(R.string.cancel, cancelCallback);
         return builder.create();
     }
 }
