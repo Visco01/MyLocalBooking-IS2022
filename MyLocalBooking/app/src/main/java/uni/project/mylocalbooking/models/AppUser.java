f20faeadd0a71f1c389ad0cf98b7600892d19917
Pietro Visconti <visconti373@gmail.com>
2022-11-23 12:09:09 +0100

    Added password getter and setter
    This change is necessary for login method

--- MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/models/AppUser.java
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/models/AppUser.java
@@ -8,17 +8,19 @@ public abstract class AppUser extends DatabaseModel {
     public final String firstname;
     public final String lastname;
     public final LocalDate dob;
+    public final String password;
 
-    public AppUser(Long id, String cellphone, String email, String firstname, String lastname, LocalDate dob) {
+    public AppUser(Long id, String cellphone, String email, String firstname, String lastname, LocalDate dob, String password) {
         super(id);
         this.cellphone = cellphone;
         this.email = email;
         this.firstname = firstname;
         this.lastname = lastname;
         this.dob = dob;
+        this.password = password;
     }
 
-    public AppUser(String cellphone, String email, String firstname, String lastname, LocalDate dob) {
-        this(null, cellphone, email, firstname, lastname, dob);
+    public AppUser(String cellphone, String email, String firstname, String lastname, LocalDate dob, String password) {
+        this(null, cellphone, email, firstname, lastname, dob, password);
     }
 }
