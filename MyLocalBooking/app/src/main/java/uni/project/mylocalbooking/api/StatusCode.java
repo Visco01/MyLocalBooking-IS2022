d3bc46a740aacdaa203ff0538157bcf05ab73497
Pietro Visconti <visconti373@gmail.com>
2022-11-27 12:33:39 +0100

    Added StatusCode enum
    This enum helps to classify the statuses of api responses

---
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/api/StatusCode.java
@@ -0,0 +1,15 @@
+package uni.project.mylocalbooking.api;
+
+public enum StatusCode {
+    OK,
+    CREATED,
+    ACCEPTED,
+    NO_CONTENT,
+    FOUND,
+    NOT_FOUND,
+    BAD_REQUEST,
+    UNAUTHORIZED,
+    UNPROCESSABLE_ENTITY,
+    INTERNAL_SERVER_ERROR,
+    JSONOBJECT_PARSE_ERROR;
+}
