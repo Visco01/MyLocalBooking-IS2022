508c6b96c926f32394edb806157b5caa66b9a8ec
Pietro Visconti <visconti373@gmail.com>
2022-12-01 14:40:39 +0100

    Modified StatusCode enum: added new code and removed the useless ones

--- MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/api/StatusCode.java
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/api/StatusCode.java
@@ -1,15 +1,12 @@
 package uni.project.mylocalbooking.api;
 
 public enum StatusCode {
-    OK,
-    CREATED,
-    ACCEPTED,
     NO_CONTENT,
-    FOUND,
     NOT_FOUND,
     BAD_REQUEST,
     UNAUTHORIZED,
     UNPROCESSABLE_ENTITY,
     INTERNAL_SERVER_ERROR,
-    JSONOBJECT_PARSE_ERROR;
+    JSONOBJECT_PARSE_ERROR,
+    SESSION_PREFERENCES_NOT_FOUND;
 }
