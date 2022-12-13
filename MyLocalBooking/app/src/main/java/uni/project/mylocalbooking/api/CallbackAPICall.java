73503d69ff6fa6d5e9ea8f33988234d663e891cf
Nicola Marizza <nicola.marizza@gmail.com>
2022-12-13 03:05:00 +0100

    Removed deprecated code

--- MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/api/CallbackAPICall.java
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/api/CallbackAPICall.java
@@ -4,35 +4,14 @@ import android.util.Log;
 
 public class CallbackAPICall<T> extends APICall<T> {
     protected RunOnResponse<T> runOnResponse;
-    private Boolean callLock = false;
 
     public CallbackAPICall(String jwt, String method, String requestBody, String url, RunOnResponse<T> runOnResponse, boolean isArray) {
         super(jwt, method, requestBody, url, isArray);
         this.runOnResponse = runOnResponse;
     }
 
-    public void run() {
-        call();
-    }
-
-    public synchronized void waitNode() throws InterruptedException {
-        while(!callLock)
-            wait();
-    }
-
     @Override
     public void call() {
-        final Object thisInstance = this;
-        runOnResponse = (response) -> {
-            if (runOnResponse != null)
-                runOnResponse.onResponse(response);
-
-            synchronized (thisInstance) {
-                callLock = true;
-                notify();
-            }
-        };
-
         super.call(runOnResponse, error -> Log.i("APICall error", error.toString()));
     }
 }
