ddf389f54b520ebff7232c3c376cce0156d03614
mircomellara <mirco.mellara@gmail.com>
2022-12-13 20:47:12 +0100

    Customizable error message on Success/Failure Fragments + checks
    Implemented some checks on certain activity + the customizable popup fragments

--- MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/activities/provider/ChangeNStrikesActivity.java
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/activities/provider/ChangeNStrikesActivity.java
@@ -1,6 +1,7 @@
 package uni.project.mylocalbooking.activities.provider;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +12,8 @@ import android.widget.EditText;
 import uni.project.mylocalbooking.MyLocalBooking;
 import uni.project.mylocalbooking.R;
 import uni.project.mylocalbooking.activities.client.ProfileClientActivity;
+import uni.project.mylocalbooking.fragments.FailureFragment;
+import uni.project.mylocalbooking.fragments.SuccessFragment;
 
 public class ChangeNStrikesActivity extends AppCompatActivity {
 
@@ -21,23 +24,40 @@ public class ChangeNStrikesActivity extends AppCompatActivity {
 
         EditText newNStrikes = findViewById(R.id.newNStrikes);
         newNStrikes.setHint("2");
-
-        // TODO: non va
         Button confirm = findViewById(R.id.confirm_change_nStrikes);
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String inputValue = newNStrikes.getText().toString();
-                if ((Integer.parseInt(inputValue)) < 1){
-                    // TODO: replace with an error popup
-                    System.out.println("Invalid Input < 1");
-                    startActivity(new Intent(MyLocalBooking.getAppContext(), ProfileClientActivity.class));
+                if (inputValue.isEmpty()){
+                    failure2();
+                }
+                else if ((Integer.parseInt(inputValue)) < 1){
+                    failure();
                 }
                 else {
-                    System.out.println("Good input" + inputValue);
-                    // Update backend
+                    // TODO: update backend
+                    success();
                 }
             }
         });
+    }
+
+    public void success() {
+        DialogFragment newFragment = new SuccessFragment("N° Strikes changed successfully!",
+                "Your number of strikes needed before an automatic blacklist occurs was updated");
+        newFragment.show(getSupportFragmentManager(), "success");
+    }
+
+    public void failure(){
+        DialogFragment newFragment = new FailureFragment("Error, invalid input!",
+                "The provided input is not valid, please choose a number greater than 1");
+        newFragment.show(getSupportFragmentManager(), "failure");
+    }
+
+    public void failure2(){
+        DialogFragment newFragment = new FailureFragment("Error, invalid input!",
+                "The provided input is not valid, please choose a NUMBER greater than 1");
+        newFragment.show(getSupportFragmentManager(), "failure");
     }
 }
