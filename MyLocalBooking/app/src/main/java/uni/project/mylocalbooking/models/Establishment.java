6cfdf77e61cc3523f1a642945396a1d585fff4ca
Nicola Marizza <nicola.marizza@gmail.com>
2022-12-29 13:34:19 +0100

    Establishment

--- MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/models/Establishment.java
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/models/Establishment.java
@@ -1,8 +1,22 @@
 package uni.project.mylocalbooking.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Collection;
 
 public class Establishment extends DatabaseModel {
+    public static final Parcelable.Creator<Establishment> CREATOR
+            = new Parcelable.Creator<Establishment>() {
+        public Establishment createFromParcel(Parcel in) {
+            return new Establishment(in);
+        }
+
+        public Establishment[] newArray(int size) {
+            return new Establishment[size];
+        }
+    };
+
     public final Provider provider;
     public final String name;
     public final String address;
@@ -31,5 +45,24 @@ public class Establishment extends DatabaseModel {
 
     public Establishment(Provider provider, String name, String address, Coordinates position, String placeId) {
         this(null, provider, name, address, position, placeId);
+    }
+
+    protected Establishment(Parcel in) {
+        super(in);
+        provider = in.readParcelable(Provider.class.getClassLoader());
+        name = in.readString();
+        address = in.readString();
+        position = in.readParcelable(Coordinates.class.getClassLoader());
+        placeId = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        super.writeToParcel(parcel, i);
+        parcel.writeParcelable(provider, i);
+        parcel.writeString(name);
+        parcel.writeString(address);
+        parcel.writeParcelable(position, i);
+        parcel.writeString(placeId);
     }
 }
