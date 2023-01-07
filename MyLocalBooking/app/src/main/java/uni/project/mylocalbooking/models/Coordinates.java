6d862612bf00ae8eee57baafa6f37a0e507da202
Nicola Marizza <nicola.marizza@gmail.com>
2023-01-07 16:42:42 +0100

    Parcelable Coordinates

--- MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/models/Coordinates.java
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/models/Coordinates.java
@@ -1,8 +1,22 @@
 package uni.project.mylocalbooking.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.android.gms.maps.model.LatLng;
 
-public class Coordinates {
+public class Coordinates implements Parcelable {
+    public static final Parcelable.Creator<Coordinates> CREATOR
+            = new Parcelable.Creator<Coordinates>() {
+        public Coordinates createFromParcel(Parcel in) {
+            return new Coordinates(in);
+        }
+
+        public Coordinates[] newArray(int size) {
+            return new Coordinates[size];
+        }
+    };
+
     public final double latitude;
     public final double longitude;
 
@@ -11,6 +25,22 @@ public class Coordinates {
         this.longitude = longitude;
     }
 
+    protected Coordinates(Parcel in) {
+        latitude = in.readDouble();
+        longitude = in.readDouble();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeDouble(latitude);
+        parcel.writeDouble(longitude);
+    }
+    
     public com.google.maps.model.LatLng toApiCoordinates() {
         return new com.google.maps.model.LatLng(latitude, longitude);
     }
