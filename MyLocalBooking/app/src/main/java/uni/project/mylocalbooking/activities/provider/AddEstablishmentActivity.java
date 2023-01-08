c0e25ab1edbae78f752396701a6648542e9627b2
mircomellara <mirco.mellara@gmail.com>
2023-01-08 13:46:40 +0100

    Add Establishment done and updated

--- MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/activities/provider/AddEstablishmentActivity.java
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/activities/provider/AddEstablishmentActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -29,6 +30,10 @@ public class AddEstablishmentActivity extends AppCompatActivity {
     private FloatingActionButton backButton;
     private Button confirmButton;
     private boolean success = false;
+    private float lat, lon;
+    private static long id = 100; //100 Default value
+    private String mapAddress;
+
 
     // TODO: position picker
 
@@ -45,39 +50,44 @@ public class AddEstablishmentActivity extends AppCompatActivity {
             }
         });
 
+        EditText name = findViewById(R.id.newEstName);
+        TextView pos = findViewById(R.id.newEstAddress);
+
+
         confirmButton = findViewById(R.id.confirm_add_est);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText name = findViewById(R.id.newEstName);
-                EditText pos = findViewById(R.id.newEstAddress);
                 String inputName = name.getText().toString();
                 String inputPos = pos.getText().toString();
-
                 // Checks
                 if (inputName.length() < 3 || inputPos.length() < 3){
                     success = false;
                     failedAdd();
+                }
+                else if (lat == 0.0f || lon == 0.0f){
+                    success = false;
+                    failedAddPos();
                 }
                 else{
                     success = true;
                 }
 
                 if (success){
+                    System.out.println(mapAddress + "\n");
                     IMyLocalBookingAPI api = IMyLocalBookingAPI.getApiInstance();
                     api.addEstablishment(new Establishment(
                             (Provider) MyLocalBooking.getCurrentUser(),
-                            inputName, inputPos,
-                            // TODO: replace with real coords and scaling ID
-                            new Coordinates(45.4408f, 12.3154f), //near Venice coords
-                            "0001"),
+                            inputName, mapAddress,
+                            // TODO: Check placeId
+                            new Coordinates(lat, lon),
+                            nextId()),
                             (a) -> confirmAdd(),
                             (b) -> {
                                         System.out.println("Data elaboration error");
                                         failedAdd();
                                     }
                     );
-                    //confirmAdd();
                 }
 
             }
@@ -91,6 +101,11 @@ public class AddEstablishmentActivity extends AppCompatActivity {
                         Intent data = result.getData();
                         SelectableMapLocation location = (SelectableMapLocation) data.getExtras().getParcelable("selected_location");
                         // TODO: do something with this data
+                        //System.out.println("Lat: " + location.coordinates.latitude + "\n" + "Long: " + location.coordinates.longitude);
+                        lat = (float) location.coordinates.latitude;
+                        lon = (float) location.coordinates.longitude;
+                        mapAddress = location.address;
+                        pos.setText(location.address);
                     }
                 });
 
@@ -110,6 +125,17 @@ public class AddEstablishmentActivity extends AppCompatActivity {
         DialogFragment newFragment = new FailureFragment("Added establishment failed",
                 "At least one of the fields is not valid, please try again with a different input");
         newFragment.show(getSupportFragmentManager(), "failedAddEstablishment");
+    }
+
+    private void failedAddPos(){
+        DialogFragment newFragment = new FailureFragment("Added establishment failed",
+                "Please pick a position on the map! ###PLACEHOLDER###");
+        newFragment.show(getSupportFragmentManager(), "failedAddEstablishment");
+    }
+
+    private static String nextId(){
+        id++;
+        return Long.toString(id);
     }
 
 }
