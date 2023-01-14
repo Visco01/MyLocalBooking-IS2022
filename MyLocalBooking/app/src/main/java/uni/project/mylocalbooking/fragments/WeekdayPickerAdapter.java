2a20fd88913f353bedbebb5d37c8ce0974945c41
Nicola Marizza <nicola.marizza@gmail.com>
2023-01-14 10:14:11 +0100

    Highlight weekday on click
    Click behavior depends on mode

--- MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/fragments/WeekdayPickerAdapter.java
+++ MyLocalBooking/app/src/main/java/uni/project/mylocalbooking/fragments/WeekdayPickerAdapter.java
@@ -1,22 +1,19 @@
 package uni.project.mylocalbooking.fragments;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.TextStyle;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Locale;
 
 import uni.project.mylocalbooking.R;
@@ -25,6 +22,7 @@ public class WeekdayPickerAdapter extends RecyclerView.Adapter<WeekdayPickerAdap
 
     private final IWeekdayPickerListener listener;
     private final LocalDate minStartOfWeek;
+    private final HashMap<LocalDate, View> selectedDates = new HashMap<>();
 
     public WeekdayPickerAdapter(IWeekdayPickerListener listener, LocalDate minStartOfWeek) {
         this.listener = listener;
@@ -36,7 +34,7 @@ public class WeekdayPickerAdapter extends RecyclerView.Adapter<WeekdayPickerAdap
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.week, parent, false);
-        return new ViewHolder(view, listener);
+        return new ViewHolder(view);
     }
 
 
@@ -57,29 +55,66 @@ public class WeekdayPickerAdapter extends RecyclerView.Adapter<WeekdayPickerAdap
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(@NonNull View itemView, IWeekdayPickerListener listener) {
+        private final LinearLayout weekRoot;
+
+        public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            LinearLayout weekRoot = (LinearLayout) itemView.getRootView();
+            weekRoot = (LinearLayout) itemView.getRootView();
 
             for(int i = 0; i < weekRoot.getChildCount(); i++) {
                 View dayView = weekRoot.getChildAt(i);
 
                 final DayOfWeek dow = DayOfWeek.of(i + 1);
-                String name = dow.getDisplayName(TextStyle.SHORT, Locale.ENGLISH);
-                ((TextView) dayView.findViewById(R.id.weekday_name)).setText(name);
-                ((Button) dayView.findViewById(R.id.weekday_button)).setOnClickListener(view -> {
+                TextView dayText = dayView.findViewById(R.id.weekday_name);
+                dayText.setText(dow.getDisplayName(TextStyle.SHORT, Locale.ENGLISH));
+            }
+        }
+
+        public void setStartOfWeek(LocalDate startOfWeek) {
+            for(int i = 0; i < weekRoot.getChildCount(); i++) {
+                View dayView = weekRoot.getChildAt(i);
+                int dayOfMonth = startOfWeek.plusDays(i + 1).getDayOfMonth();
+                ((TextView) dayView.findViewById(R.id.weekday_number)).setText(Integer.toString(dayOfMonth));
+                AppCompatButton button = (AppCompatButton) dayView.findViewById(R.id.weekday_button);
+
+                LocalDate date = startOfWeek.plusDays(i);
+                if(selectedDates.containsKey(date)) {
+                    selectedDates.put(date, dayView); // overwrite view which may be old
+                    setViewActive(dayView, true);
+                }
+                else
+                    setViewActive(dayView, false);
+
+                DayOfWeek dow = DayOfWeek.of(i + 1);
+                button.setOnClickListener(view -> {
+                    toggleView(dayView, date);
                     listener.onDaySelected(dow);
                 });
             }
         }
 
-        public void setStartOfWeek(LocalDate startOfWeek) {
-            LinearLayout weekRoot = (LinearLayout) itemView.getRootView();
+        private void toggleView(View view, LocalDate date) {
+            if(!selectedDates.containsKey(date)) { // view is not active, make active
+                selectedDates.values().forEach(v -> setViewActive(v, false));
+                selectedDates.clear();
+                setViewActive(view, true);
+                selectedDates.put(date, view);
+            }
+        }
 
-            for(int i = 0; i < weekRoot.getChildCount(); i++) {
-                int dayOfMonth = startOfWeek.plusDays(i + 1).getDayOfMonth();
-                TextView textView = ((TextView) weekRoot.getChildAt(i).findViewById(R.id.weekday_number));
-                textView.setText(Integer.toString(dayOfMonth));
+        private void setViewActive(View view, boolean active) {
+            TextView dayText = view.findViewById(R.id.weekday_name);
+            TextView numText = view.findViewById(R.id.weekday_number);
+            AppCompatButton button = (AppCompatButton) view.findViewById(R.id.weekday_button);
+
+            if(active) {
+                button.setBackgroundResource(R.drawable.circle);
+                dayText.setTextColor(0xFF000000);
+                numText.setTextColor(0xFF000000);
+            } else {
+                button.setBackgroundColor(0x00000000);
+                dayText.setTextColor(0xFFFFFFFF);
+                numText.setTextColor(0xFFFFFFFF);
             }
         }
     }
